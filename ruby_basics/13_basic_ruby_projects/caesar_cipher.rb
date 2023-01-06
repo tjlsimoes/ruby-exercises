8cf0a2322455cd464c53371f172584aec927460f
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-01-06 18:31:15 +0000

    Complete caesar_cipher

--- ruby_basics/13_basic_ruby_projects/caesar_cipher.rb
+++ ruby_basics/13_basic_ruby_projects/caesar_cipher.rb
@@ -1,7 +1,7 @@
+def caesar_cipher(string, key)
+
 alphabet_array = ("a".."z").to_a 
 alphabet_array_cap = ("A".."Z").to_a 
-
-string = "hello"
 
 string_array = string.split("")
 
@@ -9,15 +9,21 @@ string_array.reduce("") do |new_string, element|
 
    if (alphabet_array.include?(element))
     index = alphabet_array.index(element)
-    new_string + alphabet_array[index + 2]
+    new_index = index + key
+    new_index = (((new_index) % 25) - 1) if new_index > 25
+    new_string + alphabet_array[new_index]
 
    elsif (alphabet_array_cap.include?(element))
     index = alphabet_array_cap.index(element)
-    new_string + alphabet_array_cap[index + 2]
+    new_index = index + key
+    new_index = (((new_index) % 25) - 1) if new_index > 25
+    new_string + alphabet_array_cap[new_index]
 
    else
     new_string + element 
 
    end
+
+end
 
 end
