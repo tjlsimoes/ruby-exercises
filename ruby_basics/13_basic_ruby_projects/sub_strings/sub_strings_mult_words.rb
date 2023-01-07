dc26d9c5fbd45bb098e01110ee57e66e8ebc8601
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-01-07 18:42:29 +0000

    Establish final working scheme for sub_strings
    Missing, possibly among other things:
    -Turning code into method

--- ruby_basics/13_basic_ruby_projects/sub_strings/sub_strings_mult_words.rb
+++ ruby_basics/13_basic_ruby_projects/sub_strings/sub_strings_mult_words.rb
@@ -1,4 +1,27 @@
 string = "Howdy partner, sit down! How's it going?"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-string_words = string.split
+string_words = string.downcase.split
+
+the_substrings = []
+
+j = 0
+
+while j < dictionary.length 
+
+    i = 0
+    while i < string_words.length
+        if (string_words[i].include?(dictionary[j].downcase) == true)
+            the_substrings.push(dictionary[j])
+        end
+        i += 1
+    end
+
+    j += 1
+end
+
+the_substrings.reduce(Hash.new(0)) do |hash_substrings, element|
+    hash_substrings[element] += 1
+    hash_substrings
+  end
+
