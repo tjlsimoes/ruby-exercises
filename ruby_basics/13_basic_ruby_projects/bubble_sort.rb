6d38a4d77eb63c9799dcfac0ae6f9adac39a043f
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-01-09 16:34:41 +0000

    Complete bubble_sort
    Missing, among other things:
    - Turn code into method.
    - Refactor to be more efficient.

---
+++ ruby_basics/13_basic_ruby_projects/bubble_sort.rb
@@ -0,0 +1,23 @@
+array = [4,3,78,2,0,2]
+
+  
+    j = 0
+    while j < array.length do
+
+        i = 0
+        while i < (array.length - 1) do
+
+            if array[i] > array[i + 1]
+                greater = array[i]
+                smaller = array[i + 1]
+                array[i] = smaller
+                array[i + 1] = greater
+            end
+
+            i += 1
+        end
+
+        j += 1
+    end
+
+array
